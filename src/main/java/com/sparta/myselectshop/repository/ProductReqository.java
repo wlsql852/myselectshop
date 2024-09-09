0c43d67ca736efe696da6355d3a98a0b5a84b01b
wlsql852 <wlsql852@naver.com>
2024-09-09 16:12:02 +0900

    스케줄 기능 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductReqository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductReqository.java
@@ -2,6 +2,8 @@ package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductReqository extends JpaRepository<Product, Long> {
 }
