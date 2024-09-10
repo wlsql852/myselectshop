60cacaac16ab31d0112fe4304987ea4dfed8e383
wlsql852 <wlsql852@naver.com>
2024-09-10 21:36:24 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshop/entity/Folder.java
@@ -0,0 +1,36 @@
+package com.sparta.myselectshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "product")
+    private List<ProductFolder> productFolderList = new ArrayList<>();
+
+    public Folder(String name, User user) {
+        this.name = name;
+        this.user = user;
+    }
+}
