70514187919f317c405e17d787c8635b371e6d67
wlsql852 <wlsql852@naver.com>
2024-09-12 11:12:12 +0900

    Controller 테스트

---
+++ src/test/java/com/sparta/myselectshop/mvc/MockSpringSecurityFilter.java
@@ -0,0 +1,26 @@
+package com.sparta.myselectshop.mvc;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+public class MockSpringSecurityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        //SecurityContextHolder(인증객체를 담은 공간)에 인증 객체 넣기 -> 가짜 필터 만듬
+        SecurityContextHolder.getContext()
+                .setAuthentication((Authentication) ((HttpServletRequest) req).getUserPrincipal());
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        SecurityContextHolder.clearContext();
+    }
+}
